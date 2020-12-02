a7af44711a2cf8abdce814cdabef9b903982ebe4
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2020-12-01 23:12:31 -0500

    added routes folder and started api and html routes

---
+++ routes/api-routes.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+app.post("/api/workouts", (req, res) => {
+    console.log("New Workout has been addded")
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+ app.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(6)
+    .then(dbWorkouts => {
+      console.log(dbWorkouts)
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+app.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const logger  = require("morgan");
+const mongoose = require("mongoose");
+const db = require("./models");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
